34d7a8eebcd211556b32ad21cfe960c2cd3738fa
Luminoid <14302010006@fudan.edu.cn>
2018-04-13 00:29:03 +0800

    chore(Script): add command return value check

--- deploy.sh
+++ deploy.sh
@@ -9,17 +9,23 @@ commit_message=$1
 
 echo "$ hexo clean"
 hexo clean
+
 echo "$ hexo algolia"
-hexo algolia
+if ! hexo algolia; then
+    echo "Error! Algolia failed"
+    exit 2
+fi
 
 if [ ! -d "docs" ]; then
     echo "Error! No docs/ folder generated"
-    exit 2
+    exit 3
 fi
 
 echo "$ git add"
 git add -A
+
 echo "$ git commit"
 git commit -m "$commit_message"
+
 echo "$ git push"
 git push
